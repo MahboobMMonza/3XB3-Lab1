7f1aaedcc9bc21541a33f68db1613bc7fbb9587b
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-09-19 17:02:30 -0400

    Create experiment 1 file

--- experiment1.py
+++ experiment1.py
@@ -3,20 +3,30 @@ from utilities import *
 
 
 def run_experiment1():
-    # Set parameters for the experiment
+    # Run a warmup routine on the sorting algorithms to reduce cache miss errors, etc
     print('Warming up')
     warmup_sorters([insertion_sort, bubble_sort, selection_sort])
+    # Run tests
     print('Starting tests')
-    max_n, reps, bmk = 5000, 100, 10
+    max_n, reps, bmk = 4000, 15, 10
+    print('Starting Insertion Sort')
     insertion_sizes, times_insertion = incrementing_list_size_tests(max_n, reps, bmk, insertion_sort)
+    print('Starting Bubble Sort')
     bubble_sizes, times_bubble = incrementing_list_size_tests(max_n, reps, bmk, bubble_sort)
+    print('Starting Selection Sort')
     selection_sizes, times_selection = incrementing_list_size_tests(max_n, reps, bmk, selection_sort)
     # Plot results in graph
-    x_vals = [insertion_sizes, bubble_sizes, selection_sizes]
+    x_vals = insertion_sizes
     y_vals = [times_insertion, times_bubble, times_selection]
     legend_labels = ["Insertion Sort", "Bubble Sort", "Selection Sort"]
     print('Creating plots')
-    create_plot(x_vals, y_vals, "List Sizes", "Average Time (s)", legend_labels)
+    create_plot(x_vals,
+                y_vals,
+                legend_labels,
+                "Average Run Times of Selection, Insertion and Bubble Sorts",
+                f"{bmk} increments up to sizes of {max_n} with {reps} repetitions per size",
+                "List Sizes",
+                "Average Time (s)")
 
 
 if __name__ == '__main__':
