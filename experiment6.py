8c1deea3bf349c97333ed2182f989ee9c2ad35eb
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-09-24 17:15:59 -0400

    Update label name for dual quicksort

--- experiment6.py
+++ experiment6.py
@@ -7,7 +7,7 @@ def experiment6() -> None:
     name = 'Quick Sort'
     sorters = [quicksort, dual_quicksort]
     legend_labels = [name.lower().replace(' ', ''),
-                     'dual_pivot_' + name.lower().replace(' ', '')]
+                     'dual_' + name.lower().replace(' ', '')]
     compare_plot_sorters(sorters, legend_labels, name, 100000, 20, 20, 2.25)
 
 
