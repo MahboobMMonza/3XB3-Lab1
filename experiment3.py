dd7d3d0c3c058cf1b0e82661f5022bda2cc54b8f
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-09-27 19:04:44 -0400

    Conduct experiment 3

--- experiment3.py
+++ experiment3.py
@@ -1,34 +1,38 @@
-import math
-
 from bad_sorts import bubble_sort, insertion_sort, selection_sort
 from utilities import *
 
 
 def run_experiment3():
-    swap_values = []
     bubble_times = []
     selection_times = []
     insertion_times = []
-
-    reps = 15
-    length = [100, 500, 1000, 2000]
+    reps = 25
+    length = 500
+    # Fast way to calculate integer log_2 of a number
+    log_len = length.bit_length() - 1
+    # Get 50% of the swaps needed from statistically random swaps value
+    # since this is the interesting range
+    max_swaps = (length * log_len // 2) * 50 // 100
+    step_size = 40
+    num_swaps = [x for x in range(0, max_swaps + 1, step_size)]
     warmup_sorters([bubble_sort, selection_sort, insertion_sort])
 
-    for size in length:
-        swaps = size*math.log(size)/2
-        swap_values.append(swaps)
-        times_insertion = repeated_size_sorts(size, reps, insertion_sort,True, create_near_sorted_list)
-        insertion_times.append(times_insertion)
-        times_bubble = repeated_size_sorts(size, reps, bubble_sort,True, create_near_sorted_list)
-        bubble_times.append(times_bubble)
-        times_selection = repeated_size_sorts(size, reps, selection_sort,True, create_near_sorted_list)
-        selection_times.append(times_selection)
+    for swaps in num_swaps:
+        times_insertion = repeated_size_sorts(length, reps, insertion_sort, swaps)
+        insertion_times.append(sum(times_insertion) / reps)
+        times_bubble = repeated_size_sorts(length, reps, bubble_sort, swaps)
+        bubble_times.append(sum(times_bubble) / reps)
+        times_selection = repeated_size_sorts(length, reps, selection_sort, swaps)
+        selection_times.append(sum(times_selection) / reps)
 
-    create_plot(swap_values, [insertion_times, bubble_times, selection_times],
-                ['Quick Sort', 'Merge Sort', 'Heap Sort'],
-                'Execution Time vs. Number of Swaps',
-                f'{reps} runs per list length',
-                'Number of Swaps', 'Average Execution Time (s)')
+    create_plot(num_swaps, [insertion_times, bubble_times, selection_times],
+                ['Insertion Sort', 'Bubble Sort', 'Selection Sort'],
+                'Average Execution Times of Bubble, Insertion and Selection Sorts on a Nearly Sorted List',
+                f'{max_swaps // step_size + 1} increments of up to {max_swaps} swaps on a list size of' +
+                f' {length} with {reps} repetitions per swap count',
+                'Number of Swaps',
+                'Average Execution Time (s)',
+                2)
 
 
 if __name__ == '__main__':
