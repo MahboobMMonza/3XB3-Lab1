dea91dd6804f5fb98875ba127971261389006a87
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-09-30 05:29:43 -0400

    Use library function to calculate the swapping times for exp5

--- experiment5.py
+++ experiment5.py
@@ -3,29 +3,24 @@ from utilities import *
 
 
 def run_experiment5():
-    quick_times = []
-    merge_times = []
-    heap_times = []
     reps = 50
-    length = 500
+    length = 800
     log_len = length.bit_length() - 1
-    max_swaps = min((length * log_len // 2) * 50 // 100, 60)
-    step_size = 2
-    num_swaps = [x for x in range(0, max_swaps + 1, step_size)]
+    # Go up to 3% the number of swaps from statistically random list
+    max_swaps = (length * log_len // 2) * 3 // 100
+    bmk = 30
     warmup_sorters([quicksort, mergesort, heapsort])
+    print('Starting Quick Sort')
+    quick_times = incrementing_swaps_tests(max_swaps, length, reps, bmk, quicksort)[1]
+    print('Starting Merge Sort')
+    merge_times = incrementing_swaps_tests(max_swaps, length, reps, bmk, mergesort)[1]
+    print('Starting Heap Sort')
+    swaps, heap_times = incrementing_swaps_tests(max_swaps, length, reps, bmk, heapsort)
 
-    for swaps in num_swaps:
-        times_heap = repeated_size_sorts(length, reps, heapsort, swaps)
-        heap_times.append(sum(times_heap) / reps)
-        times_quick = repeated_size_sorts(length, reps, quicksort, swaps)
-        quick_times.append(sum(times_quick) / reps)
-        times_merge = repeated_size_sorts(length, reps, mergesort, swaps)
-        merge_times.append(sum(times_merge) / reps)
-
-    create_plot(num_swaps, [heap_times, quick_times, merge_times],
+    create_plot(swaps, [heap_times, quick_times, merge_times],
                 ['Heap Sort', 'Quick Sort', 'Merge Sort'],
                 'Average Execution Times of Quick, Merge and Heap Sorts on a Nearly Sorted List',
-                f'{max_swaps // step_size + 1} increments of up to {max_swaps} swaps on a list size of' +
+                f'{bmk + 1} increments of up to {max_swaps} swaps on a list size of' +
                 f' {length} with {reps} repetitions per swap count',
                 'Number of Swaps',
                 'Average Execution Time (s)',
